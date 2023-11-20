fff599f2947c7da0f97048cd1c32ceedc4fea08c
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2023-11-20 11:38:40 -0300

    styles: responsive

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <WeatherFormContextProvider>
       <main className="flex justify-center h-screen w-screen items-center">
         <div
-          className="flex flex-col gap-2   p-4 rounded-3xl h-3/4 mt-3 w-2/4  shadow-2xl from-[#023047] from-60%  to-[#106778] 
+          className="flex flex-col gap-2   p-4 rounded-3xl h-3/4 mt-3 w-2/4 max-lg:w-2/3 max-md:w-3/4 max-sm:w-4/5  shadow-2xl from-[#023047] from-60%  to-[#106778] 
           bg-gradient-to-tl"
         >
           <TiWeatherPartlySunny size={50} className="text-yellow-500" />
